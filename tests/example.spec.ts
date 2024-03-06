6dd9521cbd94bd7f435a2b34d0c8c6f946f9956b
Yukai Huang <yukaihuangtw@gmail.com>
2024-03-06 21:55:45 +0800

    initial commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,42 @@
+import { test, expect } from '@playwright/test';
+import fs from 'fs'
+import path from 'path'
+
+const downloadDir = path.join(__dirname, 'downloads');
+
+test('Go crawl each landchg results', async ({ page }) => {
+  await page.goto('https://landchg.tcd.gov.tw/Module/RWD/Web/pub_exhibit.aspx')
+  await page.waitForLoadState('networkidle')
+
+  const yearOptions = await page.$$('select[name="ProjectYear"] option')
+  const cityOptions = await page.$$('select[name="City"] option')
+
+  let years = await Promise.all(yearOptions.map(async (option) => {
+    return await option.evaluate((node) => node.getAttribute('value'))
+  }))
+  years = years.filter(year => year !== '-1')
+
+  let cities = await Promise.all(cityOptions.map(async (option) => {
+    return await option.evaluate((node) => node.getAttribute('value'))
+  }))
+  cities = cities.filter(city => city !== '-1')
+
+  console.log(years, cities)
+
+  // then loop through each year and cities
+  // and save the html source down
+  for (const year of years) {
+    for (const city of cities) {
+      await page.selectOption('select[name="ProjectYear"]', year)
+      await page.selectOption('select[name="City"]', city)
+
+      await page.waitForLoadState('networkidle')
+      console.log(`crawl ${year}-${city}`)
+
+      // await page.waitForTimeout(5000)
+      await page.innerHTML('html', { timeout: 0 }).then((html) => {
+        fs.writeFileSync(path.join(downloadDir, `${year}-${city}.html`), html)
+      })
+    }
+  }
+})
